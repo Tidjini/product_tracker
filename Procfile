ee89dba045f054c69ec8abd71c05a5ebdb319cce
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-10 13:44:00 +0100

    Add Production DB

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
+release: python manage.py migrate --settings=config.settings.production
 web: gunicorn config.wsgi --log-file -
--- config/settings/local.py
+++ config/settings/local.py
@@ -5,3 +5,9 @@ from .base import *
 DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.local.sqlite3",
+    }
+}
