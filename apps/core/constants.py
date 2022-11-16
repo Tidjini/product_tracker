7751e61990a12df474aeead12c8ed6a28d03a752
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-16 10:24:32 +0100

    set remote notification end

--- apps/core/constants.py
+++ apps/core/constants.py
@@ -1,2 +1,11 @@
-NOTIFICATION_PUSH_END = "http://localhost:7777/notify/"
-NOTIFICATION_LISTENER = "http://localhost:7777/"
+import os
+
+env = os.environ.get("ENV")
+
+if env == "development":
+    NOTIFICATION_PUSH_END = "http://localhost:7777/notify/"
+    NOTIFICATION_LISTENER = "http://localhost:7777/"
+
+else:
+    NOTIFICATION_PUSH_END = "https://eassalnotif.herokuapp.com//notify/"
+    NOTIFICATION_LISTENER = "https://eassalnotif.herokuapp.com/"
