5d08fe853ece45cdfcb2af5db07f3e0df66ee9a0
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-21 15:23:59 +0100

    change media

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.core.api_urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
